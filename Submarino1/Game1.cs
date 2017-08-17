a60d143b0c29a3c32e77d0e70209cbf14c45f01d
Sabino <fgsabino@gmail.com>
2017-08-17 23:44:33 +0000

    adding submarines

--- Submarino1/Game1.cs
+++ Submarino1/Game1.cs
@@ -9,10 +9,25 @@ namespace Submarino1
         // Declare video graphics card
         GraphicsDeviceManager graphics;
 
+        // Default sprite
         SpriteBatch spriteBatch;
 
         // Declare background image
         Texture2D background;
+
+        // Declare submarines
+        Texture2D blueSubmarine;
+        Texture2D redSubmarine;
+        Texture2D greenSubmarine;
+
+        // Declare keyboard
+        KeyboardState state;
+
+        // Declare submarines positions
+        Vector2 centeredPosition;
+        Vector2 blueSubmarinePosition;
+        Vector2 redSubmarinePosition;
+        Vector2 greenSubmarinePosition;
 
         public Game1()
         {
@@ -26,6 +41,11 @@ namespace Submarino1
             // Set screen width and height
             graphics.PreferredBackBufferWidth = 1024;
             graphics.PreferredBackBufferHeight = 768;
+
+            centeredPosition = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
+            blueSubmarinePosition = centeredPosition;
+            redSubmarinePosition = centeredPosition + new Vector2(100, 100);
+            greenSubmarinePosition = centeredPosition - new Vector2(100, 100);
 
             // Set to fullscreen
             // graphics.IsFullScreen = true;
@@ -41,8 +61,14 @@ namespace Submarino1
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // Load background image
+            // Load images
             background = Content.Load<Texture2D>("fig_fundo");
+            blueSubmarine = Content.Load<Texture2D>("fig_subazul");
+            redSubmarine = Content.Load<Texture2D>("fig_subvermelho");
+            greenSubmarine = Content.Load<Texture2D>("fig_subverde");
+
+            // Instantiate Keyboard
+            state = Keyboard.GetState();
         }
 
         protected override void UnloadContent()
@@ -70,11 +96,21 @@ namespace Submarino1
             // Draw image
             spriteBatch.Draw(background, Vector2.Zero, Color.White);
 
+            // Draw blue submarine
+            spriteBatch.Draw(blueSubmarine, blueSubmarinePosition, Color.White);
+
+            // Draw red submarine
+            spriteBatch.Draw(redSubmarine, redSubmarinePosition, Color.White);
+
+            // Draw green submarine
+            spriteBatch.Draw(greenSubmarine, greenSubmarinePosition, Color.White);
+
             // End drawing process
             spriteBatch.End();
 
 
             base.Draw(gameTime);
         }
+
     }
 }
