37b96f4ffb04a7d68e14624b2aec751f889b2de4
Sabino <fgsabino@gmail.com>
2017-08-31 15:00:44 -0300

    adding class

--- Submarino1/Game1.cs
+++ Submarino1/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
 
 namespace Submarino1
 {
@@ -20,14 +23,29 @@ namespace Submarino1
         Texture2D redSubmarine;
         Texture2D greenSubmarine;
 
-        // Declare keyboard
-        KeyboardState state;
-
         // Declare submarines positions
         Vector2 centeredPosition;
         Vector2 blueSubmarinePosition;
         Vector2 redSubmarinePosition;
         Vector2 greenSubmarinePosition;
+
+        // Declare keyboard
+        KeyboardState keyboard;
+
+        // Declare music
+        Song music;
+
+        // Declare torpedo and its position
+        Texture2D torpedo;
+        Vector2 torpedoPosition;
+
+        int torpedoMaxPosition;
+
+        Boolean isShooting;
+        Boolean isTorpedoVisible;
+        Boolean createEnemy;
+
+        List<Torpedo> Torpedos;
 
         public Game1()
         {
@@ -43,9 +61,18 @@ namespace Submarino1
             graphics.PreferredBackBufferHeight = 768;
 
             centeredPosition = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
+
             blueSubmarinePosition = centeredPosition;
             redSubmarinePosition = centeredPosition + new Vector2(100, 100);
             greenSubmarinePosition = centeredPosition - new Vector2(100, 100);
+
+            torpedoPosition = centeredPosition;
+            isShooting = false;
+            isTorpedoVisible = false;
+            torpedoMaxPosition = graphics.PreferredBackBufferWidth;
+
+            Torpedos = new List<Torpedo>();
+
 
             // Set to fullscreen
             // graphics.IsFullScreen = true;
@@ -66,9 +93,14 @@ namespace Submarino1
             blueSubmarine = Content.Load<Texture2D>("fig_subazul");
             redSubmarine = Content.Load<Texture2D>("fig_subvermelho");
             greenSubmarine = Content.Load<Texture2D>("fig_subverde");
+            torpedo = Content.Load<Texture2D>("fig_torpedo");
 
-            // Instantiate Keyboard
-            state = Keyboard.GetState();
+            // Load and play background music
+            music = Content.Load<Song>("mus_sonar");
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.Play(music);
+
+
         }
 
         protected override void UnloadContent()
@@ -81,7 +113,79 @@ namespace Submarino1
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // Instantiate Keyboard
+            keyboard = Keyboard.GetState();
+
+            //Random rnd = new Random();
+            //redSubmarinePosition.X += rnd.Next(-5, 5);
+            //redSubmarinePosition.Y += rnd.Next(-5, 5);
+            
+            if (isShooting)
+            {
+                isTorpedoVisible = true;
+                torpedoPosition.X += 10;
+                if (torpedoPosition.X >= torpedoMaxPosition)
+                {
+                    isTorpedoVisible = false;
+                    isShooting = false;
+                }
+            }
+            else
+            {
+                torpedoPosition.X = blueSubmarinePosition.X;
+                torpedoPosition.Y = blueSubmarinePosition.Y + 15;
+            }
+
+            if(keyboard.IsKeyDown(Keys.N)) {
+                createEnemy = true;
+            }
+
+            if (createEnemy) {
+                // Create some enemies
+                for (int i = 0; i < 5; i++)
+                {
+                    torpedoPosition.X = blueSubmarinePosition.X;
+                    torpedoPosition.Y = blueSubmarinePosition.Y + 10;
+                    Torpedos.Add(new Torpedo(i, torpedoPosition, torpedo, torpedoMaxPosition, true, spriteBatch));
+                }
+            }
+
+            if (keyboard.IsKeyDown(Keys.Space))
+                isShooting = true;
+
+            if (keyboard.IsKeyDown(Keys.P))
+                MediaPlayer.Pause();
+
+            if (keyboard.IsKeyDown(Keys.Up))
+                blueSubmarinePosition.Y -= 10;
+     
+            if (keyboard.IsKeyDown(Keys.Down))
+                blueSubmarinePosition.Y += 10;
+
+            if (keyboard.IsKeyDown(Keys.Right))
+                blueSubmarinePosition.X += 10;
+
+            if (keyboard.IsKeyDown(Keys.Left))
+                blueSubmarinePosition.X -= 10;
+
+            if (blueSubmarinePosition.X > 1024)
+                blueSubmarinePosition.X = -97;
+
+            if (blueSubmarinePosition.X < -97)
+                blueSubmarinePosition.X = 1024;
+
+            if (blueSubmarinePosition.Y < 0)
+                blueSubmarinePosition.Y = 0;
+
+            if (blueSubmarinePosition.Y > 720)
+                blueSubmarinePosition.Y = 720;
+
+            if (blueSubmarinePosition.X < 0)
+                blueSubmarinePosition.X = 0;
+
+            if (blueSubmarinePosition.X > 927)
+                blueSubmarinePosition.X = 927;
+
 
             base.Update(gameTime);
         }
@@ -93,17 +197,13 @@ namespace Submarino1
             // Start drawing process
             spriteBatch.Begin();
 
-            // Draw image
-            spriteBatch.Draw(background, Vector2.Zero, Color.White);
+            for (int i = 0; i < Torpedos.Count; i++)
+            {
+                Torpedos[i].Draw(spriteBatch);
+                Torpedos.Remove(Torpedos[i]);
+            }
 
-            // Draw blue submarine
-            spriteBatch.Draw(blueSubmarine, blueSubmarinePosition, Color.White);
 
-            // Draw red submarine
-            spriteBatch.Draw(redSubmarine, redSubmarinePosition, Color.White);
-
-            // Draw green submarine
-            spriteBatch.Draw(greenSubmarine, greenSubmarinePosition, Color.White);
 
             // End drawing process
             spriteBatch.End();
---
+++ Submarino1/Torpedo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Submarino1
+{
+
+    class Torpedo
+    {
+        public int TorpedoId { get; set; }
+        public Vector2 TorpedoPosition { get; set; }
+        public Texture2D TorpedoImage { get; set; }
+        public int TorpedoMaxPosition { get; set; }
+        public bool TorpedoVisible { get; set; }
+
+        public Torpedo(int id, Vector2 position, Texture2D image, int maxPosition, bool visible, SpriteBatch spriteBatch)
+        {
+            TorpedoId = id;
+            TorpedoPosition = position;
+            TorpedoImage = image;
+            TorpedoMaxPosition = maxPosition;
+            TorpedoVisible = visible;
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(TorpedoImage, TorpedoPosition, Color.White);
+        }
+    }
+
+}
